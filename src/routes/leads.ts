89e4f08f9352304ee0229f3e96c5b90352628daa
WhiteHanClean <whitehanclean@gmail.com>
2023-10-15 19:31:40 +0600

    add input

--- src/routes/leads.ts
+++ src/routes/leads.ts
@@ -66,25 +66,27 @@ router.post(
         ],
       });
 
-      const { data } = await amoCrmAxios.post("/leads", {
-        name: [body.fullName],
-        account_id: contact?.account_id,
-        custom_fields_values: [
-          {
-            field_id: customContactFields.phoneNumber.id,
-            values: [
-              {
-                value: body.phoneNumber,
-                // enum_id: customContactFields.phoneNumber.enums.WORK,
-              },
-              {
-                value: body.comment,
-                // enum_id: customContactFields.phoneNumber.enums.WORK,
-              },
-            ],
-          },
-        ],
-      });
+      const { data } = await amoCrmAxios.post("/leads", [
+        {
+          name: [body.fullName],
+          account_id: contact?.account_id,
+          custom_fields_values: [
+            {
+              field_id: customContactFields.phoneNumber.id,
+              values: [
+                {
+                  value: body.phoneNumber,
+                  // enum_id: customContactFields.phoneNumber.enums.WORK,
+                },
+                {
+                  value: body.comment,
+                  // enum_id: customContactFields.phoneNumber.enums.WORK,
+                },
+              ],
+            },
+          ],
+        },
+      ]);
 
       console.log("response:", data);
 
